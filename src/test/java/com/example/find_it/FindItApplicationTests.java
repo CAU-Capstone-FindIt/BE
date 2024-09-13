09c95f073f716bc2b291c37b94220eb249d64ef6
roganyc <roganyc@naver.com>
2024-09-13 16:49:42 +0900

    [chore] 초기 세팅 완료

---
+++ src/test/java/com/example/find_it/FindItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.find_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
