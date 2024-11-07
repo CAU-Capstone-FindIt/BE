d9992d911207db498f7d50954773bac7f3494a10
roganyc <roganyc@naver.com>
2024-11-07 20:27:03 +0900

    [fix] gradle 수정

--- src/main/java/com/example/find_it/service/CustomUserDetailsService.java
+++ src/main/java/com/example/find_it/service/CustomUserDetailsService.java
@@ -1,35 +1,35 @@
-package com.example.find_it.service;
-
-import com.example.find_it.domain.User;
-import com.example.find_it.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.Collections;
-
-@Service
-@RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
-
-    private final UserRepository userRepository;
-
-    /**
-     * authId를 사용해 사용자 정보를 불러오는 메서드
-     *
-     * @param authId 카카오 인증 ID
-     * @return UserDetails - Spring Security 인증을 위한 객체
-     * @throws UsernameNotFoundException authId에 해당하는 사용자가 없을 경우 예외 발생
-     */
-    @Override
-    public UserDetails loadUserByUsername(String authId) throws UsernameNotFoundException {
-        User user = userRepository.findByAuthId(authId)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with authId: " + authId));
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getAuthId(), "", Collections.emptyList()
-        );
-    }
-}
+//package com.example.find_it.service;
+//
+//import com.example.find_it.domain.User;
+//import com.example.find_it.repository.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Collections;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    private final UserRepository userRepository;
+//
+//    /**
+//     * authId를 사용해 사용자 정보를 불러오는 메서드
+//     *
+//     * @param authId 카카오 인증 ID
+//     * @return UserDetails - Spring Security 인증을 위한 객체
+//     * @throws UsernameNotFoundException authId에 해당하는 사용자가 없을 경우 예외 발생
+//     */
+//    @Override
+//    public UserDetails loadUserByUsername(String authId) throws UsernameNotFoundException {
+//        User user = userRepository.findByAuthId(authId)
+//                .orElseThrow(() -> new UsernameNotFoundException("User not found with authId: " + authId));
+//
+//        return new org.springframework.security.core.userdetails.User(
+//                user.getAuthId(), "", Collections.emptyList()
+//        );
+//    }
+//}
